38bf09fe9c17c35cafc04f585f149019a37a443f
witkowski01@wp.pl <your_email@example.com>
2014-04-04 10:50:35 +0200

    Wstępne FatFS
    Obsługa USB wycięta, ponieważ grzeje płytkę.

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio>
+#include<string.h>
+#include "stm32f4xx.h"
+#include "ff.c"
+
+///////////////////////////////////////////////////
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+///////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+//Biblioteki do obs³ugi usb wyciête (Grzej¹ p³ytkê) !!!!
+/////////////////////////////////////////////////
+
+//Zmienne globalne do obs³ugi
+static FILINFO fno;
+static DIR dir;
+static FIL myFile;
+
+char lfnBuffer[512];
+
+char sBuffer[256];
+char sBuffer2Write[1024];
+
+int main(void)
+{
+
+	//uint8_t write_ok = 0;
+
+	FRESULT res;
+	char *path = "";
+	char *fn; // This function is assuming non-Unicode cfg.
+
+	// ÿ CPU
+	SystemInit();
+
+
+
+    while(1)
+    {
+    }
+}
